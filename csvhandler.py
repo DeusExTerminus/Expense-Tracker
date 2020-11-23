093b0866b76c9d4be150636eab89f64c059e561e
Anthony D. Wotton <a.d.wotton@gmail.com>
2020-11-23 17:35:23 +0000

    Create csvhandler.py
    Creates csvhandler.py
    Imports csv
    Add csvhandler.py placeholder methods: csvhandler(), csvcollater(), csvwriter()

---
+++ csvhandler.py
@@ -0,0 +1,16 @@
+import csv
+
+
+# This class will handle reading, writing and manipulation of all csv formatted financial statements
+class csvhandler:
+    # will accept csv filename and path and returns a dict containing csv
+    def csvreader(strFileName, strPath):
+        return
+
+    # will collate two similar csv dicts and return as a single dict
+    def csvcollater(dictPrimary, dictSecondary):
+        return
+
+    # will accept filename and path to save dict to csv file
+    def csvwriter(strFileName, strPath):
+        return
