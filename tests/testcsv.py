f262c07f9a2800c9b1434779d3178d32243f015c
Anthony D. Wotton <a.d.wotton@gmail.com>
2020-11-25 18:06:07 +0000

    Initial csvreader test
    Adds TestCSVHandler testcase
    Adds test_csvreader test
    Adds statement to launch tests

--- tests/testcsv.py
+++ tests/testcsv.py
@@ -1,1 +1,17 @@
 import unittest
+from unittest import mock
+from csvhandler import csvhandler
+
+
+class TestCSVHandler(unittest.TestCase):
+    # May be worth changing to mock out use of the third part csv module
+    # This could remove the need for an actual test file
+    def test_csvreader(self):
+        D1 = {'type': 'CC'}
+        dictCSV = csvhandler.csvreader("/tests", "testfile.csv")
+        D2 = next(next(dictCSV))
+        self.assertDictEqual(D1, D2)
+
+
+if __name__ == "__main__":
+    unittest.main()
